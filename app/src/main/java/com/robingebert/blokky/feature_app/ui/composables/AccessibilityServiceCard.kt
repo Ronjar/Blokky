3a84f4eda71da4f036f6c2f1d0fb219ae27f2bb0
Ronjar <robingebert99@gmail.com>
2025-02-01 15:01:42 +0100

    Added Accessibility Service permission explanation dialog

--- app/src/main/java/com/robingebert/blokky/feature_app/ui/composables/AccessibilityServiceCard.kt
+++ app/src/main/java/com/robingebert/blokky/feature_app/ui/composables/AccessibilityServiceCard.kt
@@ -2,43 +2,61 @@ package com.robingebert.blokky.feature_app.ui.composables
 
 import android.content.Intent
 import android.provider.Settings
-import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.OpenInNew
 import androidx.compose.material.icons.rounded.Accessibility
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
 
 @Composable
 fun AccessibilityServiceCard(
     isAccessibilityGranted: Boolean
 ) {
-    val context = LocalContext.current
-
+    var showAccessibilityServiceDialog by remember { mutableStateOf(false) }
 
     val update: (Boolean) -> Unit = {
-        context.startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
-        Toast.makeText(context, "Click on \"Blokky\" in the \"Downloaded Apps\" Tab. Then click the slider", Toast.LENGTH_LONG).show()
+        showAccessibilityServiceDialog = true
     }
 
     Card(
@@ -92,7 +110,104 @@ fun AccessibilityServiceCard(
             )
         }
     }
+
+    if (showAccessibilityServiceDialog) {
+        AccessibilityServiceDialog {
+            showAccessibilityServiceDialog = false
+        }
+    }
 }
+
+@Composable
+fun AccessibilityServiceDialog(
+    onDismissRequest: () -> Unit
+) {
+    val context = LocalContext.current
+    var site by remember { mutableIntStateOf(0) }
+    val density = LocalDensity.current
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .animateContentSize(),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp).height(200.dp),
+            ) {
+                Text(text = "Accessibility Service", style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.height(4.dp))
+                Row{
+                    AnimatedVisibility(
+                        site == 0,
+                        exit = slideOutHorizontally() + fadeOut()
+                    ) {
+                        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.SpaceBetween) {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = "In order to block reels, Accessibility Service needs to be enabled.\nI promise to only use it for this purpose.",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            TextButton(
+                                onClick = { site = 1 }
+                            ) {
+                                Text("Show me how")
+                            }
+                        }
+
+                    }
+                    AnimatedVisibility(
+                        site == 1,
+                        enter = slideInHorizontally {
+                            with(density) { -40.dp.roundToPx() }
+                        } + fadeIn(
+                            initialAlpha = 0.3f
+                        ),
+                    ) {
+                        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = "After you opened accessibility settings, click on \"Blokky\" in the \"Downloaded Apps\" Tab. Then click the slider to enable/disable the service.",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Button(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 4.dp),
+                                onClick = {
+                                    context.startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+                                    onDismissRequest()
+                                }
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text("Open Accessibility Settings")
+                                    Spacer(modifier = Modifier.width(4.dp))
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Rounded.OpenInNew,
+                                        contentDescription = null
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AccessibilityDialogPreview() {
+    AccessibilityServiceDialog {}
+}
+
 
 @Preview
 @Composable
